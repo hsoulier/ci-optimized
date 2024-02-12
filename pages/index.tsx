0a9ad50e456334265bcbb0e09aa58a8ded936823
hsoulier <hi.soulier@gmail.com>
2024-02-12 20:39:55 +0100

    👷 Feat: Update CI + fix eslint errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       className="flex min-h-screen flex-col items-center justify-between p-24"
     >
       <Logo/>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         Hello
       </div>
     </main>
