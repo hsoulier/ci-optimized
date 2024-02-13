a38e9c4628c32b0a36f1b0570f87761880c90dff
hsoulier <hi.soulier@gmail.com>
2024-02-13 20:04:36 +0100

    👷 Feat: Create action to setup the nodejs apart

--- utils/two.test.ts
+++ utils/two.test.ts
@@ -1,6 +1,9 @@
-import { double, triple } from "./two";
+import { simple, double, triple } from "./two";
 
 describe("utils", () => {
+  it("Simple", () => {
+    expect(simple(4)).toBe(8);
+  })
   it("Double", () => {
     expect(double(4)).toBe(8);
   })
--- utils/two.ts
+++ utils/two.ts
@@ -1,3 +1,5 @@
 export const double = (num: number): number => num * 2;
 
 export const triple = (num: number): number => num * 3;
+
+export const simple = (num: number): number => num * 3;
